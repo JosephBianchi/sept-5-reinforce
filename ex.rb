62189a42a8c5bcdb20da834a8fd71c30ee7d9308
Joe Bianchi <joseph.bianchi3@gmail.com>
2017-09-05 14:30:28 -0400

    changed int key to symbol key

--- ex.rb
+++ ex.rb
@@ -2,13 +2,13 @@ hash = {}
 
 (1..50).to_a.each do |n|
   if n % 2 == 0 && n % 7 == 0
-    hash[n] = (n * 2)
+    hash[n.to_s] = (n * 2)
   elsif n % 7 == 0
-    hash[n] = (n - 1)
+    hash[n.to_s] = (n - 1)
   elsif n % 2 == 0
-    hash[n] = (n + 1)
+    hash[n.to_s] = (n + 1)
   else
-    hash[n] = n
+    hash[n.to_s] = n
   end
 end
 
